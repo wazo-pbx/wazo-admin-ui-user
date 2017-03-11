bf19ebd3881acecf1b535554c070c9b7b94f1af8
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-03-10 21:40:51 -0500

    add more options

--- user/form.py
+++ user/form.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms.fields import SubmitField
 from wtforms.fields import TextField
 from wtforms.fields import BooleanField
+from wtforms.fields.html5 import EmailField
 
 from wtforms.validators import InputRequired
 from wtforms.validators import Optional
@@ -15,4 +16,14 @@ from wtforms.validators import Optional
 class UserForm(FlaskForm):
     firstname = TextField('Firstname', [InputRequired()])
     lastname = TextField('Lastname')
+    extension = TextField('Extension')
+    email = EmailField('Email')
+    mobile_phone_number = TextField('Phone mobile')
+    ring_seconds = TextField('Ring seconds')
+    music_on_hold = TextField('Music on Hold')
+    preprocess_subroutine = TextField('Subroutine')
+    simultaneous_calls = TextField('Simultaneous calls')
+    timezone = TextField('Timezone')
+    userfield = TextField('User Field')
+    description = TextField('Description')
     submit = SubmitField('Submit')
