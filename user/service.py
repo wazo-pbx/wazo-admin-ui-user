81f554c33f207dd80e7ac487323a0ab3945546de
Francois Blackburn <blackburnfrancois@gmail.com>
2017-03-22 14:11:49 -0400

    service: add fallbacks

--- user/service.py
+++ user/service.py
@@ -10,6 +10,19 @@ class UserService(BaseConfdService):
     resource_name = 'user'
     resource_confd = 'users'
 
-
     def list(self, limit=None, order=None, direction=None, offset=None, search=None):
-        return self._confd.users.list(view='summary', search=search, order=order, limit=limit, direction=direction, offset=offset)
+        return self._confd.users.list(view='summary',
+                                      search=search,
+                                      order=order,
+                                      limit=limit,
+                                      direction=direction,
+                                      offset=offset)
+
+    def update(self, resources):
+        super(UserService, self).update(resources)
+
+        user = resources.get('user', {})
+        if not user.get('fallbacks'):
+            return
+
+        self._confd.users(user['uuid']).update_fallbacks(user['fallbacks'])
