9b7c0e0a2c2c9acd2e9416bb9c48b073b1d92a13
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-21 13:41:59 -0400

    plugin: add a package.sh file for wazo-plugind

---
+++ package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+case "$1" in
+    install)
+        make install
+    ;;
+
+    uninstall)
+        make uninstall
+    ;;
+
+    *)
+        echo "$0 called with unknown argument '$1'" >&2
+        exit 1
+    ;;
+esac
