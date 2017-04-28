657d1ad8f42fdde0663744c71af5eba5ef14e1ce
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-28 14:37:13 -0400

    package.sh: update python package build and installation

--- package.sh
+++ package.sh
@@ -4,17 +4,19 @@
 
 case "$1" in
     build)
-        python setup.py sdist --formats=zip
+        python setup.py bdist_egg
         ;;
 
     install)
-        pip install dist/wazo_admin_ui_user-*.zip
+        easy_install dist/wazo_admin_ui_user-*-py2.7.egg
         cp etc/wazo-admin-ui/conf.d/user.yml /etc/wazo-admin-ui/conf.d
         systemctl restart wazo-admin-ui
         ;;
 
     uninstall)
-        make uninstall
+        rm -rf /usr/local/lib/python2.7/dist-packages/wazo_admin_ui_user-*-py2.7.egg
+        rm -f /etc/wazo-admin-ui/conf.d/user.yml
+        systemctl restart wazo-admin-ui
         ;;
 
     *)
