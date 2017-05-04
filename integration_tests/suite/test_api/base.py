324c9b9d3da82a713cb5179b3f8f16afedc3d1ff
Francois Blackburn <fblackburn@wazo.community>
2017-05-04 15:29:10 -0400

    add integration tests

---
+++ integration_tests/suite/test_api/base.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import os
+
+from xivo_test_helpers.admin_ui.asset_launching_test_case import AdminUIAssetLaunchingTestCase
+
+from .pages.user import UserListPage
+
+ASSET_ROOT = os.path.join(os.path.dirname(__file__), '..', '..', 'assets')
+
+
+class IntegrationTest(AdminUIAssetLaunchingTestCase):
+
+    assets_root = ASSET_ROOT
+
+    @classmethod
+    def setup_browser(cls):
+        browser = AdminUIAssetLaunchingTestCase.setup_browser()
+        browser.pages['users'] = UserListPage
+        return browser
