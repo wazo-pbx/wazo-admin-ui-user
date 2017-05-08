79a6d2d363e6d8d272302ce039028bbfd23d994a
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-05-08 10:55:43 -0400

    rename top level package
    the name of the top level package is the one that will appear in /usr/local/lib/python2.7/dist-packages
    and user is already used. The naming scheme is wazo_plugind_<name_with_underscope>_<namespace> to match
    the debian package names

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     entry_points={
         'wazo_admin_ui.plugins': [
-            'user = user.plugin:Plugin',
+            'user = wazo_plugind_admin_ui_user_official.plugin:Plugin',
         ]
     }
 )
