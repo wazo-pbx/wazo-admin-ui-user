f7f9b8aa1d3545c4c6bf88f5fb5fecb8761a5e74
Francois Blackburn <fblackburn@wazo.community>
2017-06-15 13:11:27 -0400

    int tests: reset provd on start
    reason: the reset initialise default config, with registrar 'default'

--- integration_tests/suite/test_api/base.py
+++ integration_tests/suite/test_api/base.py
@@ -5,6 +5,7 @@
 import os
 
 from xivo_test_helpers.admin_ui.asset_launching_test_case import AdminUIAssetLaunchingTestCase
+from xivo_test_helpers.confd import provd
 
 from .pages.user import UserListPage
 
@@ -14,6 +15,11 @@ ASSET_ROOT = os.path.join(os.path.dirname(__file__), '..', '..', 'assets')
 class IntegrationTest(AdminUIAssetLaunchingTestCase):
 
     assets_root = ASSET_ROOT
+
+    @classmethod
+    def setUpClass(cls):
+        super(IntegrationTest, cls).setUpClass()
+        provd.reset()
 
     @classmethod
     def setup_browser(cls):
